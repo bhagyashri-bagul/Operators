c4add8d18b5fe2c496195b619caea7c4deb83b61
bhagyashri <104320703+bhagyashri-bagul@users.noreply.github.com>
2024-04-27 22:40:57 +0530

    Arithmetic Operator

---
+++ Arithmatic.java
@@ -0,0 +1,11 @@
+class Arithmatic {
+    public static void main(String args[]){
+int a=5;
+int b=5;
+System.out.println("Addition ="+ (a+b));
+System.out.println("Subtract ="+ (a-b));
+System.out.println("Multiply ="+ (a*b));
+System.out.println("Divide =" + (a/b));
+System.out.println("Modulo =" +(a%b));
+    }
+}
