c5ada5304523403404ba6004c1b95d6a137b149e
bhagyashri <104320703+bhagyashri-bagul@users.noreply.github.com>
2024-04-27 22:42:40 +0530

    Relational Operator

---
+++ Relation.java
@@ -0,0 +1,26 @@
+class Relation{
+    public static void main(String args[]){
+        int a=10;
+        int b=5;
+        // to check a=b it returns true else it return false 
+        System.out.println("\n Equal to operator: " +(a==b));
+
+        // checking is value is not equal then it returns true else false 
+        System.out.println("\n Not equal to operator: "+(a!=b));
+
+        // greater than operator 
+        System.out.println("\n greater than operator a>b: "+(a>b));
+
+        // less than operator
+        System.out.println("\n less than operator a<b: "+(a<b));
+
+        int c=5;
+        int d=5;
+        System.out.println("\n Equal to operator : "+(c==d));
+        System.out.println("\n Not equal to operator: "+(c!=d));
+        System.out.println("\n greater than operator c>d: "+(c>d));
+        System.out.println("\n less than operator c<d: "+(c<d));
+
+
+    }
+}
