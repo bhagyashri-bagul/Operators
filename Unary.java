bf4f3a44fedaf8efe14caf9b2809cbf035781c55
bhagyashri <104320703+bhagyashri-bagul@users.noreply.github.com>
2024-04-27 22:41:34 +0530

    Unary Operator

---
+++ Unary.java
@@ -0,0 +1,22 @@
+class Unary{
+    public static void main(String args[]){
+
+        System.out.println("Pre Increment Operator: ");
+        int a=5;
+        int b=++a;
+        System.out.println("Value of a is "+ a);
+        System.out.println("Value of b is "+ b);
+
+        System.out.println("Post Increment Operator");
+        
+       
+        int c = 2;
+        int d = c++;
+
+        System.out.println("Value of c is "+ c);
+        System.out.println("Value of d is "+ d);
+
+
+
+    }
+}
