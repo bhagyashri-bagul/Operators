09ceb4aa371e30d51be4264da4418621c9673928
bhagyashri <104320703+bhagyashri-bagul@users.noreply.github.com>
2024-04-27 22:56:46 +0530

    Assignment Operator

---
+++ Assignment.java
@@ -0,0 +1,23 @@
+class Assignment{
+    public static void main(String args[]){
+       
+
+    int a=2;
+    int b=5;
+    int c=6;
+    int d=8;
+    b+=2; // it indicate that b=b+2 which is 7 
+    System.out.println(b);
+
+    c-=1; // It means  c=c-1
+    System.out.println(c);
+
+    d/=2; // d=d/2
+    System.out.println(d);
+
+    a%=2;
+    System.out.println(a);
+    
+    }
+
+}
